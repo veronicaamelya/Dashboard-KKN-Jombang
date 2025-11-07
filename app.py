8867580a5ee0323bb759ddbdb969eaa869f91792
veronicaamelya <115519337+veronicaamelya@users.noreply.github.com>
2025-11-07 14:30:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -189,24 +189,20 @@ elif selected == "Analisis":
     <h3 style='background-color:#FAF6E9; padding:8px; border-radius:10px; text-align:center;'>Perencanaan Produksi</h3>
     """, unsafe_allow_html=True)
 
-# Inisialisasi agar tidak error sebelum tabel diisi
-    total_bahan_baku = st.session_state.get("total_bahan_baku", 0.0)
-    total_operasional = st.session_state.get("total_operasional", 0.0)
-    total_investasi = st.session_state.get("total_investasi", 0.0)
 
     col1, col2, col3 = st.columns(3)
     with col1:
-        bahan_diolah = st.number_input("Jumlah Bahan yang Diolah (kg)", min_value=0.0, step=0.1, value=5.0, key="bahan_diolah")
+        bahan_diolah = st.number_input("Jumlah Bahan yang Diolah (kg)", min_value=0.0, step=0.1, value=5.0, key="bahan_diolah",on_change=update_session_state, args=("bahan_diolah"))
     with col2:
-        target_produksi = st.number_input("Target Produksi (gram)", min_value=0.0, step=1.0, key="target_produksi")
+        target_produksi = st.number_input("Target Produksi (gram)", min_value=0.0, step=1.0, key="target_produksi",on_change=update_session_state, args=("target_produksi"))
     with col3:
-        kemasan_per_produk = st.number_input("Kemasan per Produksi (gram)", min_value=0.0, step=1.0, key="kemasan_per_produk")
+        kemasan_per_produk = st.number_input("Kemasan per Produksi (gram)", min_value=0.0, step=1.0, key="kemasan_per_produk",on_change=update_session_state, args=("kemasan_per_produk"))
 
     col4, col5 = st.columns(2)
     with col4:
-        jumlah_kemasan = st.number_input("Jumlah Kemasan (pcs)", min_value=0, step=1, key="jumlah_kemasan")
+        jumlah_kemasan = st.number_input("Jumlah Kemasan (pcs)", min_value=0, step=1, key="jumlah_kemasan",on_change=update_session_state, args=("jumlah_kemasan"))
     with col5:
-        margin_laba = st.number_input("Margin Laba (%)", min_value=0.0, step=0.5, key="margin_laba")
+        margin_laba = st.number_input("Margin Laba (%)", min_value=0.0, step=0.5, key="margin_laba",on_change=update_session_state, args=("margin_laba"))
     if margin_laba > 0:
         biaya_per_kemasan = (total_bahan_baku + total_operasional) / jumlah_kemasan if jumlah_kemasan > 0 else 0
         harga_jual_otomatis = biaya_per_kemasan * (1 + margin_laba / 100)
@@ -414,7 +410,7 @@ elif selected == "Analisis":
         
         payback_period = None
 
-        diskonto_bulanan_sederhana = (1 + diskonto/100)(1/12) - 1
+        diskonto_bulanan_sederhana = (1 + diskonto/100)**(1/12) - 1
         
         cash_flows_sederhana = [-investasi_awal_sederhana] + laba_bulanan[:int(periode)]
         
@@ -463,7 +459,7 @@ elif selected == "Analisis":
         cash_flows = [-investasi_awal] + ncf
 
         # Perhitungan finansial
-        diskonto_bulanan = (1 + diskonto/100)(1/12) - 1
+        diskonto_bulanan = (1 + diskonto/100)**(1/12) - 1
         npv = npf.npv(diskonto_bulanan, cash_flows)
         irr = npf.irr(cash_flows)
         irr_percent = irr * 100 if irr is not None else 0
@@ -585,3 +581,4 @@ elif selected == "Tentang Kami":
         tepat dan berbasis data untuk meningkatkan daya saing serta keberlanjutan usahanya.
     </div>
     """, unsafe_allow_html=True)
+
